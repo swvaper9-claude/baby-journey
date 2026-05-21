613d0ccff6fb4d96969d1455650dd64023ce6924
swvaper9-claude <swvaper9@gmail.com>
2026-05-21 15:23:19 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        guide: 'guide.html',
-      }
-    }
-  }
 })
